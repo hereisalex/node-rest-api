5c12be4c908a6215ff4e5ec090787501cab68742
Alex McKenzie <here.is.alex@gmail.com>
2021-11-24 00:19:07 -0600

    add delete user and get user APIs

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 
 //update user
 router.put('/:id', async (req, res) => {
-    if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -28,7 +28,31 @@ router.put('/:id', async (req, res) => {
     }
 });
 //delete user
+router.delete('/:id', async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json('Account has been deleted');
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can only delete your own account!");
+    }
+});
 //get user
+router.get("/:id", async (req, res) => {
+    try {
+        // look up user by user id 
+        const user = await User.findById(req.params.id);
+        // seperate 'other' from password, updated at (we don't need those)
+        const { password, updatedAt, ...other} = user._doc
+        // return the stuff from 'other'
+        res.status(200).json(other);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 //follow user
 //unfollow user
 
